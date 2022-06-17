61b6bd1fca0886de7eaa85340c625b5c8d719e8a
NickTheWhale <51139062+NickTheWhale@users.noreply.github.com>
2022-06-16 20:48:58 -0500

    idk what i changed

--- fov-webcam.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'fov-webcam.exe' with Python DLL being found.
-set PATH=c:\users\loehr\appdata\local\programs\python\python39;%PATH%
-set PYTHONHOME=c:\users\loehr\appdata\local\programs\python\python39
-"%~dp0.\fov-webcam.exe" %*
